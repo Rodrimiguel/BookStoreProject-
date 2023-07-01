a3d41194358fc5f9d92bddc955c2803868065723
RoderickMMM <rodrigomfoggia84@gmail.com>
2023-07-01 00:13:30 -0300

    Update AddScoped&BranchOffice

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using BOOKSTORE00.Data;
+using BOOKSTORE00.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BookContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("BookContext") ?? throw new InvalidOperationException("Connection string 'BookContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IBranchOfficeService, BranchOfficeService>();
 
 var app = builder.Build();
 
